80c65dbb45e15ae97ae94aab3afa0eb5131af4ff
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2025-04-24 16:31:55 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -42,6 +42,26 @@ const CONFIG = {
   page: { pdfViewerClientId: 'a047957d3f3d46c08affb18ac02a1bc9' },
   hlxPage: { pdfViewerClientId: 'df45ae024b7a4a9fbeb7d306075c7749' },
   hlxLive: { pdfViewerClientId: '3a0c922fabdf4178804a3dfa410a254f' },
+  const stageDomainsMap = {
+    'news.stage.adobe.com': {
+      'www.adobe.com(?!\/content\/dam\/cc\/en\/investor-relations\/pdfs\/).*$)': 'origin',
+      'business.adobe.com': 'business.stage.adobe.com',
+      'helpx.adobe.com': 'helpx.stage.adobe.com',
+      'blog.adobe.com': 'blog.stage.adobe.com',
+      'www.adobe.com': 'www.stage.adobe.com',
+    },
+    '--news--adobecom.(hlx|aem).live': {
+      'business.adobe.com': 'business.adobe.com',
+      'helpx.adobe.com': 'helpx.adobe.com',
+      'blog.adobe.com': 'blog.adobe.com',
+    },
+    '--news--adobecom.(hlx|aem).page': {
+      'www.adobe.com(?!\/content\/dam\/cc\/en\/investor-relations\/pdfs\/).*$)': 'origin',
+      'business.adobe.com': 'business.stage.adobe.com',
+      'helpx.adobe.com': 'helpx.stage.adobe.com',
+      'blog.adobe.com': 'blog.stage.adobe.com',
+    },
+  };
   languages: {
     en: {
       ietf: 'en',
