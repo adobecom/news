ea2cce564724fc81eb5b2515dd3243a5bb23d107
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2025-04-27 20:04:07 -0700

    [MWPW-171549]Update Stage Domain Mapping for news domain (#69)
    * Update scripts.js
    
    * Update scripts.js
    
    * Update scripts.js
    
    * Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -44,23 +44,25 @@ const CONFIG = {
   hlxLive: { pdfViewerClientId: '3a0c922fabdf4178804a3dfa410a254f' },
   stageDomainsMap: {
     'news.stage.adobe.com': {
-      'www.adobe.com(?!\\/content\\/dam\\/cc\\/en\\/investor-relations\\/pdfs\\/).*$': 'www.stage.adobe.com',
+      'www.adobe.com(?!\\/*\\S*\\/(content\\/dam\\/cc\\/en\\/investor-relations\\/pdfs\\/)\\S*)': 'www.stage.adobe.com',
       'business.adobe.com': 'business.stage.adobe.com',
       'helpx.adobe.com': 'helpx.stage.adobe.com',
       'blog.adobe.com': 'blog.stage.adobe.com',
-      'news.adobe.com': 'origin',
+      'news.adobe.com': 'news.stage.adobe.com',
     },
     '--news--adobecom.(hlx|aem).live': {
       'business.adobe.com': 'business.adobe.com',
       'helpx.adobe.com': 'helpx.adobe.com',
       'blog.adobe.com': 'blog.adobe.com',
       'www.adobe.com': 'www.adobe.com',
+      'news.adobe.com': 'news.adobe.com',
     },
     '--news--adobecom.(hlx|aem).page': {
-      'www.adobe.com(?!\\/content\\/dam\\/cc\\/en\\/investor-relations\\/pdfs\\/).*$': 'www.stage.adobe.com',
+      'www.adobe.com(?!\\/*\\S*\\/(content\\/dam\\/cc\\/en\\/investor-relations\\/pdfs\\/)\\S*)': 'www.stage.adobe.com',
       'business.adobe.com': 'business.stage.adobe.com',
       'helpx.adobe.com': 'helpx.stage.adobe.com',
       'blog.adobe.com': 'blog.stage.adobe.com',
+      'news.adobe.com': 'news.stage.adobe.com',
     },
   },
   languages: {
